a726803c04cc902789ac02ba776b5609b5d0f4c7
Kanstantsin Hancharou <hancharou.kanstantsin@gmail.com>
2024-02-15 20:20:40 +0100

    FIX: Prevent scale changing
    After calling orthonormalized on a global_transform the scale's been reseting itself.
    Now the scale information is stored before calling align_with_y() function.

--- addons/snapping_system/snapping_system.gd
+++ addons/snapping_system/snapping_system.gd
@@ -1,15 +1,15 @@
 @tool
 extends EditorPlugin
 
-const RAY_LENGTH = 1000
+const RAY_LENGTH: int = 1000
 
-@onready var undo_redo = get_undo_redo()
-var undo_transform = null
-var snap_mode_toggle = false
-var by_normal = true
+var snap_mode_toggle := false
+var by_normal := true
+var undo_transform: Transform3D
+var selection := EditorInterface.get_selection()
+var selected: Node3D
 
-var selection = EditorInterface.get_selection()
-var selected : Node3D = null
+@onready var undo_redo := get_undo_redo()
 
 
 func _enter_tree() -> void:
@@ -33,6 +33,8 @@ func _forward_3d_gui_input(camera, event):
 	if Input.is_key_pressed(KEY_W) and Input.is_key_pressed(KEY_ALT) and not snap_mode_toggle:
 		snap_mode_toggle = true
 		print("snap_mode is ON")
+		print(str(selected.get_gizmos()))
+		
 	
 	if event.is_action_pressed("ui_cancel") and snap_mode_toggle:
 		snap_mode_toggle = false
@@ -47,10 +49,13 @@ func _forward_3d_gui_input(camera, event):
 		var face_normal = surface.normal
 		selected.position = surface.position
 		if by_normal:
+			var scale = selected.scale
 			selected.global_transform = align_with_y(selected.global_transform, face_normal)
+			selected.scale = scale
 	
 	if event.is_pressed() and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and snap_mode_toggle:
 		snap_mode_toggle = false
+		print("snap_mode is OFF")
 		undo_redo.create_action("Snapping to surface")
 		undo_redo.add_do_property(selected, "global_transform", selected.global_transform)
 		undo_redo.add_undo_property(selected, "global_transform", undo_transform)
